3a471bbe3ca8b5c827125c4f39518f1346921cb9
erickzorn <erick.nogueira.zorn@gmail.com>
2024-08-01 20:05:02 -0300

    Initial commit

---
+++ node.js
@@ -0,0 +1,31 @@
+module.exports = {
+    env: {
+        es2021: true,
+        node: true,
+    },
+    extends: ['standard', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+        'prettier/prettier': [
+            'error',
+            {
+                printWidth: 80,
+                tabWidth: 4,
+                singleQuote: false,
+                trailingComma: 'all',
+                arrowParens: 'always',
+                semi: true,
+            },
+        ],
+    },
+    settings: {
+        'import/parsers': {
+            [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+        },
+    },
+}
